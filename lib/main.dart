e51b67b2c9fdeefe10c5e8c79a9caf6e467e3ad7
Sandip puri <46167161+Sandippuri@users.noreply.github.com>
2020-02-04 05:52:34 -0800

    bottom nav bar ferya xa hai

--- lib/main.dart
+++ lib/main.dart
@@ -3,61 +3,83 @@ import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:namaskar_app/app_screens/homepage.dart';
 import "./app_screens/setting.dart";
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
+  int _currentTab=0;
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Namaskar",
     home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.account_circle),
+
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text(
+          "Namaskar",
+          style: TextStyle(
+            fontSize: 25.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(5),
+              child: IconButton(
+                icon: Icon(Icons.search),
+                iconSize: 30.0,
+                onPressed: () {},
+              )),
+          IconButton(
+            icon: Icon(Icons.notifications),
             iconSize: 30.0,
             onPressed: () {},
           ),
-          title: Text(
-            "Namaskar",
-            style: TextStyle(
-              fontSize: 25.0,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.indigo,
-          actions: <Widget>[
-            Padding(
-                padding: EdgeInsets.all(5),
-                child: IconButton(
-                  icon: Icon(Icons.search),
-                  iconSize: 30.0,
-                  onPressed: () {},
-                )),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              iconSize: 30.0,
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body:ListView(
-          children: <Widget>[
-            Home(),EventsAndPlaces()
-          ],
-        ),
-      bottomNavigationBar: CurvedNavigationBar(
-        color: Colors.indigo,
-        backgroundColor: Colors.white,
-        buttonBackgroundColor: Colors.white,
-        height: 50.0,
-        items: <Widget>[
-          Icon(Icons.home, size: 20.0),
-          Icon(Icons.notifications, size: 20.0),
-          Icon(Icons.bookmark, size: 20.0),
-          Icon(Icons.settings, size: 20.0),
         ],
-        animationDuration: Duration(milliseconds: 200),
-        animationCurve: Curves.easeInCirc,
+      ),
+      body:SafeArea(child:ListView(
+        children: <Widget>[
+          Home(),EventsAndPlaces()
+        ],
+      ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab,
+        onTap: (int value) {
+          setState(() {
+            _currentTab = value;
+          });
+        },
+
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 25.0,
+            ),
+            title: SizedBox.shrink(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bookmark,
+              size: 25.0,
+            ),
+            title: SizedBox.shrink(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+              size: 25.0,
+            ),
+            title: SizedBox.shrink(),
+          ),
+
+        ],
       ),
     ),
   ));
-}//main
+}
